4260300e95062164bcd3c3786ddd8ce8e264cc79
lapan1ch <vitamin.lapickis@gmail.com>
2023-01-17 02:08:52 +0200

    WTF

--- src/ThreeDigits.java
+++ src/ThreeDigits.java
@@ -2,7 +2,7 @@ public class ThreeDigits {
     public static void main(String[] args) {
         short a = 123;
         int firstDigit = a / 100;
-        int secondDigit = (a / 10) % 10;
+        int secondDigit = a / 10 % 10;
         int thirdDigit = a % 10;
         System.out.println("First digit is - " + firstDigit);
         System.out.println("Second digit is - " + secondDigit);
