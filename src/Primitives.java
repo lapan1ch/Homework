9165de2afd022c5f0b55cf9123dba9325aa32c24
lapan1ch <vitamin.lapickis@gmail.com>
2023-01-17 02:04:15 +0200

    WTF

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Primitives {
+public class Primitives {
+    public static void main(String[] args) {
+        byte a = 0;
+        short b = 1;
+        int c = 2;
+        long d = 3;
+        float e = 2.718f;
+        double pi = 3.14;
+        boolean love = true;
+        char x = 'X';
+        System.out.println("Hole from Bublik is " + a);
+        System.out.println(b + " blondy and seven black friends");
+        System.out.println("You can always make easy money, if you have at least " + c + " kidneys");
+        System.out.println("The perfect number of drinking buddies is " + d);
+        System.out.println("Most people do not know that exp equals " + e + ", actually they don't even care");
+        System.out.println(pi + "dar - sounds like Krasnodar, but without KRASNO");
+        System.out.println(love + " to the end");
+        System.out.println("Everybody's favourite shop, is a triple " + x + " shop");
+    }
 }
