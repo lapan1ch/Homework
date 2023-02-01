e38effa7e82ec34778353baf79d25f073ea285d3
lapan1ch <vitamin.lapickis@gmail.com>
2023-02-01 14:54:00 +0200

    Getting better...

--- src/AdditionalHomework/Lesson3/Dog.java
+++ src/AdditionalHomework/Lesson3/Dog.java
@@ -3,14 +3,21 @@ package AdditionalHomework.Lesson3;
 public class Dog {
     private String dogName = "Spike";
     byte age;
+    String color;
     public String getDogName() {
         return dogName;
     }
-    public void voice(){
+    public void voice() {
         System.out.println("- Woof, woof, woof!");
     }
-    public void happyBirthday(){
-        byte realAge = (byte) (age + 1);
-        System.out.println("Dog barked " + realAge + " times");
+    public void happyBirthday(Dog dog) {
+        dog.age = 8;
+        System.out.println("Dog barked " + dog.age + " times");
+    }
+    public Dog(String color) {
+        this.color = color;
+    }
+    public void changeColor(String newColor) {
+        this.color = newColor;
     }
 }
--- src/AdditionalHomework/Lesson3/DogDemo.java
+++ src/AdditionalHomework/Lesson3/DogDemo.java
@@ -2,14 +2,16 @@ package AdditionalHomework.Lesson3;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog dog = new Dog();
+        Dog dog = new Dog("Black");
         dog.age = 7;
         dog.voice();
         System.out.println("- " +dog.getDogName() + " is " + dog.age + " years old.");
-        dog.happyBirthday();
+        System.out.println(dog.color + " dog got angry");
+        dog.happyBirthday(dog);
         System.out.println("- Happy Birthday, old pal!");
         dog.voice();
-        System.out.println("- How old are you, again?");
-        dog.happyBirthday();
+        dog.changeColor("Red");
+        System.out.println("Checked my calendar, it was saying - " + dog.age + " years");
+        System.out.println(dog.color + " dog was staring at me...");
     }
 }
